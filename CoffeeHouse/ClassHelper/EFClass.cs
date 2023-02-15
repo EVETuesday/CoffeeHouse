abe6a2e34644f398cde8e16cd96e05f002b1522d
EVE <EVE@HOME-PC>
2023-02-15 10:47:41 +0300

    Добавьте файлы проекта.

---
+++ CoffeeHouse/ClassHelper/EFClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoffeeHouse.DataBase;
+
+namespace CoffeeHouse.ClassHelper
+{
+    public class EFClass
+    {
+        public static Entities Context { get;} = new Entities();
+    }
+}
