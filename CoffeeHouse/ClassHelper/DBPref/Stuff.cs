e9fc8b65846f7fb88ae51ff0886ca677bbf9bc1a
Максим Кириллин <Kirillin980@gmail.com>
2023-04-21 16:17:14 +0300

    Cart

---
+++ CoffeeHouse/ClassHelper/DBPref/Stuff.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeHouse.DataBase
+{
+    using System;
+    using System.Collections.Generic;
+
+    public partial class Stuff
+    {
+        public int Quantity { get; set; }
+    }
+}
